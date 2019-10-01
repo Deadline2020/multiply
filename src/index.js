dd96f50ebb87aae8399f6eb062b9f672ba5d5abf
Andrew <Deadline2020@yandex.by>
2019-10-01 23:50:47 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const numberA = first.split("").reverse();
+  const numberB = second.split("").reverse();
+  let resultNumber = [0];
+  let inTheMind;
+  let temp;
+  for (let a = 0; a < numberA.length; a++) {
+    inTheMind = 0;
+    for (let b = 0; b < numberB.length; b++) {
+      temp = numberA[a] * numberB[b] + inTheMind + (resultNumber[a + b] || 0);
+      resultNumber.splice(a + b, 1, temp ? temp % 10 : 0);
+      inTheMind = temp ? Math.floor(temp / 10) : 0;
+      if (b === numberB.length - 1 && inTheMind) resultNumber.splice(a + b + 1, 1, inTheMind);
+    }
+  }
+  return resultNumber.reverse().join("");
 }
